aca4521bc277fe7a61da59358dc77fe5d44e6b56
Andrew Duong <duongandrews@gmail.com>
2019-11-01 02:19:46 -0400

    Added two bars for data

--- src/db/migrations/02_test_data.sql
+++ src/db/migrations/02_test_data.sql
@@ -46,7 +46,7 @@ VALUES
 INSERT INTO venues
   (name, address, city, province, country, latitude, longitude, display_picture, phone_number, email, postal_code)
 VALUES
-  ('Early Mercy', '540 King St W', 'Toronto', 'Ontario', 'Canada', 43.6451725, -79.3979345, 'https://clubbable.blob.core.windows.net/medias/Early-Mercy-?timestamp=636900604528499736', '4165070777', 'earlymercy@hotmail.com', 'M5V 1M3');
+  ('Early Mercy', '540 King St W', 'Toronto', 'Ontario', 'Canada', 43.6451725, -79.3979345, 'https://clubbable.blob.core.windows.net/medias/Early-Mercy-?timestamp=636900604528499736', '416-507-0777', 'earlymercy@hotmail.com', 'M5V 1M3');
 
 INSERT INTO venues
   (name, address, city, province, country, latitude, longitude, display_picture, phone_number, email, postal_code)
@@ -56,42 +56,42 @@ VALUES
 INSERT INTO venues
   (name, address, city, province, country, latitude, longitude, display_picture, phone_number, email, postal_code)
 VALUES
-  ('Crocodile Rock', '240 Adelaide St W', 'Toronto', 'Ontario', 'Canada', 43.6483306, -79.3907146, 'https://images.unsplash.com/photo-1543365067-fa127bcb2303?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1070&q=80', '416599-9751', 'crocrock.ca', 'M5H 1W7');
+  ('Crocodile Rock', '240 Adelaide St W', 'Toronto', 'Ontario', 'Canada', 43.6483306, -79.3907146, 'https://images.unsplash.com/photo-1543365067-fa127bcb2303?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1070&q=80', '416-599-9751', 'help@crocrock.ca', 'M5H 1W7');
 
 INSERT INTO venues
   (name, address, city, province, country, latitude, longitude, display_picture, phone_number, email, postal_code)
 VALUES
-  ('The Fifth Social Club', '225 Richmond St W Suite 100', 'Toronto', 'Ontario', 'Canada', 43.648883, -79.3891145, 'https://images.unsplash.com/photo-1566417713940-fe7c737a9ef2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1015&q=80', '416979-3000', 'fifthsocialclub.thefifth.com', 'M5V 1W2');
-
-
-INSERT INTO venues
-  (name, address, city, province, country, latitude, longitude, display_picture, phone_number, email, postal_code)
-VALUES
-  ('Cube', '314 Queen St W', 'Toronto', 'Ontario', 'Canada', 43.6494619, -79.396457, 'https://images.unsplash.com/photo-1553267863-69f04dd0c3c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80', '416666-7740', 'cubetoronto.com', 'M5V 2A2');
+  ('The Fifth Social Club', '225 Richmond St W Suite 100', 'Toronto', 'Ontario', 'Canada', 43.648883, -79.3891145, 'https://images.unsplash.com/photo-1566417713940-fe7c737a9ef2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1015&q=80', '416-979-3000', 'fifthsocialclub@thefifth.com', 'M5V 1W2');
 
 
 INSERT INTO venues
   (name, address, city, province, country, latitude, longitude, display_picture, phone_number, email, postal_code)
 VALUES
-  ('Fiction', '180 Pearl St', 'Toronto', 'Ontario', 'Canada', 43.6475749, -79.3913, 'https://images.unsplash.com/photo-1558047291-26cc184812b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80', '647352-5505', 'fictionclub.ca', 'M5J 1L5');
+  ('Cube', '314 Queen St W', 'Toronto', 'Ontario', 'Canada', 43.6494619, -79.396457, 'https://images.unsplash.com/photo-1553267863-69f04dd0c3c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80', '416-666-7740', 'hello@cubetoronto.com', 'M5V 2A2');
 
 
 INSERT INTO venues
   (name, address, city, province, country, latitude, longitude, display_picture, phone_number, email, postal_code)
 VALUES
-  ('Everleigh', '580 King St W', 'Toronto', 'Ontario', 'Canada', 43.6447676, -79.4015213, 'https://images.unsplash.com/photo-1572024137212-f4de6e02447f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80', '647907-6673', 'theeverleigh.ca', 'M5J 1L5');
+  ('Fiction', '180 Pearl St', 'Toronto', 'Ontario', 'Canada', 43.6475749, -79.3913, 'https://images.unsplash.com/photo-1558047291-26cc184812b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80', '647-352-5505', 'fictionclub.ca', 'M5J 1L5');
 
 
 INSERT INTO venues
   (name, address, city, province, country, latitude, longitude, display_picture, phone_number, email, postal_code)
 VALUES
-  ('bar244', '244 Adelaide St W', 'Toronto', 'Ontario', 'Canada', 43.6483546, -79.3911514, 'https://images.unsplash.com/photo-1562878952-7694a555ad20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80', '416599-2442', 'bar244.ca', 'M5H 1Y3');
+  ('Everleigh', '580 King St W', 'Toronto', 'Ontario', 'Canada', 43.6447676, -79.4015213, 'https://images.unsplash.com/photo-1572024137212-f4de6e02447f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80', '647-907-6673', 'help@theeverleigh.ca', 'M5J 1L5');
 
 
 INSERT INTO venues
   (name, address, city, province, country, latitude, longitude, display_picture, phone_number, email, postal_code)
 VALUES
-  ('Cake Bar & Nightclub', '214 Adelaide St W', 'Toronto', 'Ontario', 'Canada', 43.6485944, -79.3898244, 'https://images.unsplash.com/photo-1562049070-7e003d30a3d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80', '416599-2442', 'cakebartoronto.com', 'M5H 1W7');
+  ('bar244', '244 Adelaide St W', 'Toronto', 'Ontario', 'Canada', 43.6483546, -79.3911514, 'https://images.unsplash.com/photo-1562878952-7694a555ad20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80', '416-599-2442', 'contact@bar244.ca', 'M5H 1Y3');
+
+
+INSERT INTO venues
+  (name, address, city, province, country, latitude, longitude, display_picture, phone_number, email, postal_code)
+VALUES
+  ('Cake Bar & Nightclub', '214 Adelaide St W', 'Toronto', 'Ontario', 'Canada', 43.6485944, -79.3898244, 'https://images.unsplash.com/photo-1562049070-7e003d30a3d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80', '416599-2442', 'hello@cakebartoronto.com', 'M5H 1W7');
 
 
 INSERT INTO venues
@@ -99,7 +99,16 @@ INSERT INTO venues
 VALUES
   ('Escobar', '485 King St W', 'Toronto', 'Ontario', 'Canada', 43.644798, -79.3991208, 'https://images.unsplash.com/photo-1541532713592-79a0317b6b77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', null, null, 'M5H 1W7');
 
+INSERT INTO venues
+  (name, address, city, province, country, latitude, longitude, display_picture, phone_number, email, postal_code)
+VALUES
+  ("Gatsby's Speakeasy", '82 Bathurst St', 'Toronto', 'Ontario', 'Canada', 43.6431549, -79.404923, 'http://www.blogto.com/eat_drink/2017/01/enduring-appeal-secret-bars-toronto/', '416-599-2442', 'hello@speakeasy.com', 'M5V 2P3');
 
+
+  INSERT INTO venues
+  (name, address, city, province, country, latitude, longitude, display_picture, phone_number, email, postal_code)
+VALUES
+  ("Foggy Dew Irish Pub", '803 King St W', 'Toronto', 'Ontario', 'Canada', 43.64294377731903, -79.40706269469942, 'https://ssmscdn.yp.ca/image/resize/4beeacbd-7a0f-4d34-ba8f-ee5708f4b045/ypui-d-mp-pic-gal-lg/foggy-dew-irish-alehouse-and-pub-storefront-9.jpg', '416-703-4042', 'foggydew@fabrestaurants.ca', 'M5V 1N4');
 
 --- VISITS table ---
 
