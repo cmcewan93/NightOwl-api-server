3e6e915d341a92026a45b8741626e28bc30830c1
Colin McEwan <colin-mcewan@hotmail.com>
2019-11-05 20:29:29 -0500

    added post reoute to reviews controller

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
-const { getReviews, getReviewsByVenueId, addReview } = require("../controllers/reviewsController");
+const {
+  getReviews,
+  getReviewsByVenueId,
+  addReview
+} = require("../controllers/reviewsController");
 
 router.get("/", (req, res) => {
   getReviews().then(data => {
@@ -15,20 +19,20 @@ router.get("/:id", (req, res) => {
   });
 });
 
-// router.post("/", (req, res) => {
-//   addReview().then(data => {
-//     console.log(data);
-//     res.send(data)
-//   })
-//   .catch(err => {
-//     console.error(null, err.stack)
-//   })
-// })
-// module.exports = router;
-router.post("/upload", (req, res) => {
-  console.log(req.body, "image");
-  // call to save to DB
-
-  res.send(200);
+router.post("/", (req, res) => {
+  //console.log("@#$@$@#$@#", req.body);
+  let review = req.body;
+  addReview(review)
+    .then(data => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch(err => {
+      console.error(null, err.stack);
+    });
 });
+// module.exports = router;
+// router.post("/upload", (req, res) => {
+
+// });
 module.exports = router;
